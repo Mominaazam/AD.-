16529e5a7efbb1415fa5a9e09cfa1fe3e97249a0
Harrythedevelopercs <harrykennedy.cs@gmail.com>
2024-04-30 13:31:22 +0500

    token

--- studybud/settings.py
+++ studybud/settings.py
@@ -113,13 +113,15 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-   'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
-   ),
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-   ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
+
 
 
 # Password validation
